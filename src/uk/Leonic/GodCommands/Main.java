b1d29d30e4f6124d626dd829f8d8da576f6de144
Leonic <PythonLeonic@hotmail.co.uk>
2014-02-01 18:04:30 +0000

    GodCommands By Leonic

--- src/uk/Leonic/GodCommands/Main.java
+++ src/uk/Leonic/GodCommands/Main.java
@@ -1,9 +1,13 @@
 package uk.Leonic.GodCommands;
 
 import java.util.logging.*;
+
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.*;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.*;
 import org.bukkit.plugin.java.*;
 import org.bukkit.potion.*;
@@ -73,6 +77,118 @@ public class Main extends JavaPlugin{
 			
 			}else{
 				player.sendMessage(ChatColor.RED + "Error: " + ChatColor.BOLD + "That Player Isn't Online");
+			
+			}
+			if(commandLabel.equalsIgnoreCase("godkit")){
+				if(args.length == 0){
+				try{
+				ItemStack item1 = new ItemStack(Material.BOW, 1);
+				ItemStack item2 = new ItemStack(Material.DIAMOND_SWORD, 1);
+				ItemStack item3 = new ItemStack(Material.DIAMOND_CHESTPLATE);
+				ItemStack item4 = new ItemStack(Material.DIAMOND_HELMET, 1);
+				ItemStack item5 = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+				ItemStack item6 = new ItemStack(Material.DIAMOND_BOOTS, 1);
+				
+				item1.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 999);
+				item1.addUnsafeEnchantment(Enchantment.ARROW_FIRE, 999);
+				item1.addUnsafeEnchantment(Enchantment.ARROW_KNOCKBACK, 999);
+				item1.addUnsafeEnchantment(Enchantment.ARROW_DAMAGE, 999);
+				player.getInventory().addItem(item1);
+				
+				item2.addUnsafeEnchantment(Enchantment.KNOCKBACK, 999);
+				item2.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 999);
+				item2.addUnsafeEnchantment(Enchantment.DURABILITY, 999);
+				item2.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 999);
+				item2.addUnsafeEnchantment(Enchantment.LUCK, 999);
+				player.getInventory().addItem(item2);
+				
+				item3.addUnsafeEnchantment(Enchantment.THORNS, 999);
+				item3.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 999);
+				item3.addUnsafeEnchantment(Enchantment.WATER_WORKER, 999);
+				item3.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 999);
+				player.getInventory().addItem(item3);
+				
+				item4.addUnsafeEnchantment(Enchantment.THORNS, 999);
+				item4.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 999);
+				item4.addUnsafeEnchantment(Enchantment.WATER_WORKER, 999);
+				item4.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 999);
+				player.getInventory().addItem(item4);
+				
+				item5.addUnsafeEnchantment(Enchantment.THORNS, 999);
+				item5.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 999);
+				item5.addUnsafeEnchantment(Enchantment.WATER_WORKER, 999);
+				item5.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 999);
+				player.getInventory().addItem(item5);
+				
+				item6.addUnsafeEnchantment(Enchantment.THORNS, 999);
+				item6.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 999);
+				item6.addUnsafeEnchantment(Enchantment.WATER_WORKER, 999);
+				item6.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 999);
+				player.getInventory().addItem(item6);
+				
+				}catch(Exception ex){
+					log.severe("Whoa Something Went Wrong!");
+					ex.printStackTrace();
+				}
+				}
+				else if(args.length == 1){
+					if(player.getServer().getPlayer(args[0])!=null){
+						Player targetplayer = player.getServer().getPlayer(args[0]);
+						try{
+							ItemStack item1 = new ItemStack(Material.BOW, 1);
+							ItemStack item2 = new ItemStack(Material.DIAMOND_SWORD, 1);
+							ItemStack item3 = new ItemStack(Material.DIAMOND_CHESTPLATE);
+							ItemStack item4 = new ItemStack(Material.DIAMOND_HELMET, 1);
+							ItemStack item5 = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+							ItemStack item6 = new ItemStack(Material.DIAMOND_BOOTS, 1);
+							
+							item1.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 999);
+							item1.addUnsafeEnchantment(Enchantment.ARROW_FIRE, 999);
+							item1.addUnsafeEnchantment(Enchantment.ARROW_KNOCKBACK, 999);
+							item1.addUnsafeEnchantment(Enchantment.ARROW_DAMAGE, 999);
+							targetplayer.getInventory().addItem(item1);
+							
+							item2.addUnsafeEnchantment(Enchantment.KNOCKBACK, 999);
+							item2.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 999);
+							item2.addUnsafeEnchantment(Enchantment.DURABILITY, 999);
+							item2.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 999);
+							item2.addUnsafeEnchantment(Enchantment.LUCK, 999);
+							targetplayer.getInventory().addItem(item2);
+							
+							item3.addUnsafeEnchantment(Enchantment.THORNS, 999);
+							item3.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 999);
+							item3.addUnsafeEnchantment(Enchantment.WATER_WORKER, 999);
+							item3.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 999);
+							targetplayer.getInventory().addItem(item3);
+							
+							item4.addUnsafeEnchantment(Enchantment.THORNS, 999);
+							item4.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 999);
+							item4.addUnsafeEnchantment(Enchantment.WATER_WORKER, 999);
+							item4.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 999);
+							targetplayer.getInventory().addItem(item4);
+							
+							item5.addUnsafeEnchantment(Enchantment.THORNS, 999);
+							item5.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 999);
+							item5.addUnsafeEnchantment(Enchantment.WATER_WORKER, 999);
+							item5.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 999);
+							targetplayer.getInventory().addItem(item5);
+							
+							item6.addUnsafeEnchantment(Enchantment.THORNS, 999);
+							item6.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 999);
+							item6.addUnsafeEnchantment(Enchantment.WATER_WORKER, 999);
+							item6.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 999);
+							targetplayer.getInventory().addItem(item6);
+							
+							}catch(Exception ex){
+								log.severe("Whoa Something Went Wrong!");
+								ex.printStackTrace();
+							}
+				
+				}else{
+					player.sendMessage(ChatColor.RED + "Error: " + ChatColor.BOLD + "That Player Isn't Online");
+				
+				}
+				}
 			}
 			}
 			}
